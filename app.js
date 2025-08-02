232159e5945c5625599bbad16fb8562b34ea0bcc
amardipbanerjee <amardipbanerjee2000@gmail.com>
2025-08-02 12:59:13 +0530

    test 1

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Platform",
+      "Cache-Control", // <-- Add this line
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("API running!");
+});
+
+// Start the server
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+});
